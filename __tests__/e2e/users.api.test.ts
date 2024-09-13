63c66dab2abb6cac9da1e757e2805be67364c792
Alexander Sedov <async.await.then@gmail.com>
2024-09-13 14:17:00 +0300

    fix tests

--- __tests__/e2e/users.api.test.ts
+++ __tests__/e2e/users.api.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import {app} from '../../src'
-import {HTTP_STATUS} from "../../src/consts";
-import {User} from "../../src/types";
+import {HTTP_STATUS, STATUS_MESSAGES} from "../../src/consts"
+import {User} from "../../src/types"
 
 describe('/users', () => {
+
+    // const serverListener = app.listen(3000)
+    const server = request(app)
 
     let createdUser: User | undefined;
 
@@ -14,21 +17,28 @@ describe('/users', () => {
 
     /** get users array (empty) */
     it('Should return code 200 and array of users', async () => {
-        await request(app)
+        await server
             .get('/users')
             .expect(HTTP_STATUS.OK_200, [])
     })
 
     /** get no existing user */
     it('Should return code 404 of no existing user', async () => {
-        await request(app)
+        await server
             .get('/users/99999')
             .expect(HTTP_STATUS.NOT_FOUND_404)
     })
 
+    /** create new user with incorrect data */
+    it('Should return code 400 of create new user with incorrect data', async () => {
+        await server
+            .post('/users')
+            .expect(HTTP_STATUS.BAD_REQUEST_400, STATUS_MESSAGES.BODY_EMPTY_NAME)
+    })
+
     /** create new user */
     it('Should return code 201 of create new user', async () => {
-        await request(app)
+        await server
             .post('/users')
             .send({name: 'Test User'})
             .expect(HTTP_STATUS.CREATED_201)
@@ -43,9 +53,23 @@ describe('/users', () => {
             })
     })
 
+    /** update user with incorrect data */
+    it('Should return code 400 of update user with incorrect data', async () => {
+        await server
+            .put(`/users/${createdUser?.id}`)
+            .expect(HTTP_STATUS.BAD_REQUEST_400, STATUS_MESSAGES.BODY_EMPTY_NAME)
+    })
+
+    /** update user with incorrect id */
+    it('Should return code 404 of update user with incorrect id', async () => {
+        await server
+            .put(`/users/99999`)
+            .expect(HTTP_STATUS.BAD_REQUEST_400)
+    })
+
     /** update user */
     it('Should return code 200 of update user', async () => {
-        await request(app)
+        await server
             .put(`/users/${createdUser?.id}`)
             .send({name: 'Alexander Userson'})
             .expect(HTTP_STATUS.OK_200, {id: createdUser?.id, name: 'Alexander Userson'})
@@ -53,22 +77,41 @@ describe('/users', () => {
 
     /** get users array */
     it('Should return code 200 and array of users', async () => {
-        await request(app)
+        await server
             .get('/users')
             .expect(HTTP_STATUS.OK_200, [{id: createdUser?.id ,name: 'Alexander Userson'}])
     })
 
     /** get user */
     it('Should return code 200 and user object', async () => {
-        await request(app)
+        await server
             .get(`/users/${createdUser?.id}`)
             .expect(HTTP_STATUS.OK_200, {id: createdUser?.id ,name: 'Alexander Userson'})
     })
 
+    /** get not existing user */
+    it('Should return code 404 of not existing user', async () => {
+        await server
+            .get(`/users/100500`)
+            .expect(HTTP_STATUS.NOT_FOUND_404)
+    })
+
+    /** delete not existing user */
+    it('Should return code 404 of delete not existing user', async () => {
+        await server
+            .delete(`/users/100500`)
+            .expect(HTTP_STATUS.NOT_FOUND_404)
+    })
+
     /** delete user */
     it('Should return code 204 of delete user', async () => {
-        await request(app)
+        await server
             .delete(`/users/${createdUser?.id}`)
             .expect(HTTP_STATUS.NO_CONTENT_204)
     })
+
+    afterAll(done => {
+        done()
+    })
+
 })
