e3de0b498434487d9661bacf1140873643e0a522
Alexander Sedov <async.await.then@gmail.com>
2024-09-17 15:33:54 +0300

    use routes

--- src/index.ts
+++ src/index.ts
@@ -28,77 +28,76 @@ app.get('/', (req: Request, res: Response) => {
     res.send(STATUS_MESSAGES.WELCOME)
 })
 
-app.get('/users', (req: RequestQuery<UserQueryModel>, res: Response<UsersType[]>) => {
-    let foundUsers: UsersType[] = db.users
+app.route('/users')
+    .get((req: RequestQuery<UserQueryModel>, res: Response<UsersType[]>) => {
+        let foundUsers: UsersType[] = db.users
 
-    if (req.query.name) {
-        foundUsers = foundUsers.filter(
-            user => user.name.toLowerCase().includes(req.query.name)
-        )
-    }
+        if (req.query.name) {
+            foundUsers = foundUsers.filter(
+                user => user.name.toLowerCase().includes(req.query.name)
+            )
+        }
 
-    res.status(HTTP_STATUS.OK_200).json(foundUsers.map(getUserViewModel))
-})
-
-app.get('/users/:id', (req: RequestParams<URIUserIdParamsModel>, res: Response<UsersType>) => {
-    const foundUser: UsersType | undefined = db.users.find(
-        user => user.id === +req.params.id
-    )
-
-    if (!foundUser) {
-        res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
-        return
-    }
-
-    res.status(HTTP_STATUS.OK_200).json(getUserViewModel(foundUser))
-})
-
-app.post('/users', (req: RequestBody<UserCreateModel>, res: Response<UsersType|Object>) => {
-    if (!req.body.name || !req.body.email) {
-        res.status(HTTP_STATUS.BAD_REQUEST_400).send(STATUS_MESSAGES.EMPTY_DATA)
-        return
-    }
-
-    const createdUser: UsersType =  {
-        id: +new Date(),
-        name: req.body.name,
-        email: req.body.email,
-    }
-
-    db.users.push(createdUser)
-    res.status(HTTP_STATUS.CREATED_201).json(getUserViewModel(createdUser))
-})
-
-app.delete('/users/:id', (req: RequestParams<URIUserIdParamsModel>, res: Response) => {
-    for (let i = 0; i < db.users.length; i++) {
-        if (db.users[i].id === +req.params.id) {
-            db.users.splice(i, 1)
-            res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+        res.status(HTTP_STATUS.OK_200).json(foundUsers.map(getUserViewModel))
+    })
+    .post((req: RequestBody<UserCreateModel>, res: Response<UsersType|Object>) => {
+        if (!req.body.name || !req.body.email) {
+            res.status(HTTP_STATUS.BAD_REQUEST_400).send(STATUS_MESSAGES.EMPTY_DATA)
             return
         }
-    }
 
-    res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
-})
+        const createdUser: UsersType =  {
+            id: +new Date(),
+            name: req.body.name,
+            email: req.body.email,
+        }
 
-app.put('/users/:id', (req: RequestBodyParam<URIUserIdParamsModel, UserUpdateModel>, res: Response<UsersType|Object>) => {
-    if (!req.body.name) {
-        res.status(HTTP_STATUS.BAD_REQUEST_400).send(STATUS_MESSAGES.BODY_EMPTY_NAME)
-        return
-    }
+        db.users.push(createdUser)
+        res.status(HTTP_STATUS.CREATED_201).json(getUserViewModel(createdUser))
+    })
 
-    const foundUser: UsersType | undefined = db.users.find(
-        user => user.id === +req.params.id
-    )
+app.route('/users/:id')
+    .get((req: RequestParams<URIUserIdParamsModel>, res: Response<UsersType>) => {
+        const foundUser: UsersType | undefined = db.users.find(
+            user => user.id === +req.params.id
+        )
 
-    if (!foundUser) {
+        if (!foundUser) {
+            res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
+            return
+        }
+
+        res.status(HTTP_STATUS.OK_200).json(getUserViewModel(foundUser))
+    })
+    .put((req: RequestBodyParam<URIUserIdParamsModel, UserUpdateModel>, res: Response<UsersType|Object>) => {
+        if (!req.body.name) {
+            res.status(HTTP_STATUS.BAD_REQUEST_400).send(STATUS_MESSAGES.BODY_EMPTY_NAME)
+            return
+        }
+
+        const foundUser: UsersType | undefined = db.users.find(
+            user => user.id === +req.params.id
+        )
+
+        if (!foundUser) {
+            res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
+            return
+        }
+
+        foundUser.name = req.body.name
+        res.status(HTTP_STATUS.OK_200).json(getUserViewModel(foundUser))
+    })
+    .delete((req: RequestParams<URIUserIdParamsModel>, res: Response) => {
+        for (let i = 0; i < db.users.length; i++) {
+            if (db.users[i].id === +req.params.id) {
+                db.users.splice(i, 1)
+                res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+                return
+            }
+        }
+
         res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
-        return
-    }
-
-    foundUser.name = req.body.name
-    res.status(HTTP_STATUS.OK_200).json(getUserViewModel(foundUser))
-})
+    })
 
 app.delete('/__test__/init-drop-users', (req: Request, res: Response) => {
     db.users = []
